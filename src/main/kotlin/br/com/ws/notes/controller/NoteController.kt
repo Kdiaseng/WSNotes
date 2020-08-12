6c1c7910e428842a9859d736350a91bb17220b2c
kaleb.silva <kaleb.silva@itbam.org.br>
2020-08-12 18:11:08 -0400

    use ResponseEntity in return

--- src/main/kotlin/br/com/ws/notes/controller/NoteController.kt
+++ src/main/kotlin/br/com/ws/notes/controller/NoteController.kt
@@ -4,6 +4,8 @@ import br.com.ws.notes.form.NoteForm
 import br.com.ws.notes.model.Note
 import br.com.ws.notes.service.NoteService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -19,17 +21,18 @@ class NoteController {
     }
 
     @PostMapping
-    fun add(@RequestBody note: NoteForm): Note {
-        return noteService.save(note)
+    fun add(@RequestBody note: NoteForm): ResponseEntity<Note> {
+        return ResponseEntity(noteService.save(note), HttpStatus.CREATED)
     }
 
     @PutMapping("{id}")
-    fun alter(@PathVariable id: Long, @RequestBody note: NoteForm): Note {
-        return noteService.alter(id, note)
+    fun alter(@PathVariable id: Long, @RequestBody note: NoteForm): ResponseEntity<Note> {
+        return ResponseEntity.ok(noteService.alter(id, note))
     }
 
     @DeleteMapping("{id}")
-    fun delete(@PathVariable id: Long) {
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> {
         noteService.delete(id)
+        return ResponseEntity.ok().build()
     }
 }
